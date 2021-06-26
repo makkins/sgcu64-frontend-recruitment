c1d4efb2f88588609a789fbd64095398b5bf0b02
makkins <makkpunthat@gmail.com>
2021-06-26 23:51:30 +0700

    init: Problem2 welcome_menu & check_in

---
+++ Problem #2/ChulaChana.py
@@ -0,0 +1,42 @@
+def welcome_menu():
+    print( "Welcome to Chula Chana!!! ")
+    print( "Available commands ")
+    print( "1. Check in user ")
+    print( "2. Check out user ")
+    print( "3. Print people count ")
+    x = int(input("Please input any number : "))
+    print("--------------------------------------------------------------")
+    if(x==1):
+        check_in()
+    if(x==2):
+        check_out()
+    if(x==3):
+        show_list()
+
+def check_in():
+    print("Enter phone number : ")
+    number = input()
+    print("1:Mahamakut building, \n2:Sara Phra Kaew, \n3:CU Sport Complex, \n4:Sanam Juub, \n5:Samyan Mitr Town")
+    x = int(input("Please input any number : "))
+    while(x>5 or x<1):
+        x= int(input("Invalid places number, pls input any number again : "))
+    if number not in user:
+        user[number]=x
+        places[x]+=1
+    else:
+        displaces
+
+welcome_menu()
+print("continue the program ? (y/n)")
+c=input()
+while(c=='y'):
+    welcome_menu()
+
+user = {} #key = telNumber , value = places
+places_count={1:0,2:0,3:0,4:0,5:0} # key = places , value = peopleCount
+places_name=["Mahamakut building", "Sara Phra Kaew", "CU Sport Complex", "Sanam Juub", "Samyan Mitr Town"]
+# 1:Mahamakut building,
+# 2:Sara Phra Kaew, 
+# 3:CU Sport Complex,
+# 4:Sanam Juub,
+# 5:Samyan Mitr Town
